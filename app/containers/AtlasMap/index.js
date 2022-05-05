a9d7983cd9ad9e98611d29cfd2b313bf4dee8d58
Bebo Elhosary <beboelhosary@hotmail.com>
2022-05-05 15:58:21 -0700

    Add marker on quebec

--- app/containers/AtlasMap/index.js
+++ app/containers/AtlasMap/index.js
@@ -28,39 +28,40 @@ import messages from '../NotFoundPage/messages';
 /*eslint-disable */
 function AtlasMap() {
   useEffect(() => {
-    L.map('map', {
-      center: [49.8419, 24.0315],
-      zoom: 16,
-      // style: { height: '100vh' },
-      layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution:
-            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        }),
-      ],
-    });
-  }, []);
+    //   L.map('map', {
+    //     center: [46.813877, -71.207977],
+    //     zoom: 16,
+    //     // style: { height: '100vh' },
+    //     layers: [
+    //       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //         attribution:
+    //           '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    //       }),
+    //     ],
+    //   });
+    // }, []);
+    const qbc = [46.813877, -71.207977];
 
-  //   const map = L.map('map').setView([51.505, -0.09], 13);
-  //   L.Icon.Default.mergeOptions({
-  //     // eslint-disable-next-line global-require
-  //     iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  //     // eslint-disable-next-line global-require
-  //     iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  //     // eslint-disable-next-line global-require
-  //     shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-  //   });
-  //
-  //   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  //     attribution:
-  //       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-  //   }).addTo(map);
-  //
-  //   L.marker([51.5, -0.09])
-  //     .addTo(map)
-  //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  //     .openPopup();
-  // }, []);
+    const map = L.map('map').setView(qbc, 13);
+    L.Icon.Default.mergeOptions({
+      // eslint-disable-next-line global-require
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      // eslint-disable-next-line global-require
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      // eslint-disable-next-line global-require
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    });
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(qbc)
+      .addTo(map)
+      .bindPopup('Bonjour.<br> Je me souviens.')
+      .openPopup();
+  }, []);
 
   return <div id="map" style={{ height: '100vh' }} />;
 }
